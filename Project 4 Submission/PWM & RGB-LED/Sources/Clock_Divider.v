b6f761555012a76d1b636b5d4401f2b741e3b5ec
blainehindman-eecs <60160835+blainehindman-eecs@users.noreply.github.com>
2020-11-10 11:28:31 -0800

    Update Clock_Divider.v

--- Project 4 Submission/PWM & RGB-LED/Sources/Clock_Divider.v
+++ Project 4 Submission/PWM & RGB-LED/Sources/Clock_Divider.v
@@ -1,35 +1,23 @@
 `timescale 1ns / 1ps
 
-//real digital source 
+//real digital source
 //using 4bit counter 
 module Clock_Divider(
     input clk,
     input rst,
     output reg Clock_Divider
-    );
-    
-    localparam Const = 12;
-    
+);
     reg [3:0] count;
-    
     always @ (posedge(clk), posedge(rst))
     begin
-        if (rst == 1'b1)
-            count <= 3'b0;
-        else if (count == Const - 1)
-            count <= 3'b0;
-        else
-            count <= count + 1;
+        if (rst == 1'b1) count <= 3'b0;
+        else if (count == 10 - 1) count <= 3'b0;
+        else count <= count + 1;
     end
-    
     always @ (posedge(clk), posedge(rst))
     begin
-        if (rst == 1'b1)
-            Clock_Divider <= 1'b0;
-        else if (count == Const - 1)
-            Clock_Divider <= ~Clock_Divider;
-        else
-            Clock_Divider <= Clock_Divider;
+        if (rst == 1'b1) Clock_Divider <= 1'b0;
+        else if (count == 10 - 1) Clock_Divider <= ~Clock_Divider;
+        else Clock_Divider <= Clock_Divider;
     end
-    
 endmodule
